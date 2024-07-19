d16601ef6bf13af844cc391934cbc78954f3bcfc
AlejandroRinconPerez <alejandro.ingenieria.estudios@gmail.com>
2024-07-19 17:08:07 -0500

    Actulizacion inventario Marca Talla Estadso

--- Registro_Items.py
+++ Registro_Items.py
@@ -12,12 +12,13 @@ def Registro_Prenda_Nueva():
             print("elemento Registrado anteriormente")
             return
     info_elemento = {}
-    info_elemento["descripcion"] = str(input("Ingrese la descripcion del producto"))
-    info_elemento["cantiddad"] = int(input("Cantidad de articulos ha registrar"))
-    info_elemento["costo"] =  int(input("costo del producto"))
-    info_elemento["precio"] =  int(input("Ingrese el precio de venta del producto"))
-    info_elemento["descripcion"] = str(input("Ingrese la descripcion del producto"))
-    
+    info_elemento["descripcion"] = str(input("Ingrese la descripcion del producto:  "))
+    info_elemento["cantiddad"] = int(input("Cantidad de articulos ha registrar:  "))
+    info_elemento["Marca"] = str(input("Ingrese la Marca del Producto:  "))
+    info_elemento["Talla"] = str(input("Ingrese la talla del Producto:  "))
+    info_elemento["estado"] = "Activo" #Estado por defecto Se debe modificar solido si canrtidad = 0
+    info_elemento["costo"] =  int(input("costo del producto:  "))
+    info_elemento["precio"] =  int(input("Ingrese el precio de venta del :  "))
     elemento ={}
     elemento[id_ropa]= info_elemento
     elementos.append(elemento)
@@ -25,31 +26,35 @@ def Registro_Prenda_Nueva():
     return
         
 
-def Facturas ():
-    facturas = cargar_datos_facturas()
-    id_factura = input("Id De la FACtura:  ")
-    for factura in facturas:
-        if factura.get( id_factura,None) != None:
-            print("Factura Registrada anterioermete")
-            return
-    info_Factura ={}
-    info_Factura["fecha"]=input("Fecha")
-    info_Factura["Nombre"]=input("Nombre")   
-    info_Factura["Telefono"]= input("Telefono") 
-    producto = {}
-    info_Factura[producto]={}
+# def Facturas ():
+#     facturas = cargar_datos_facturas()
+#     id_factura = input("Id De la FACtura:  ")
+#     for factura in facturas:
+#         if factura.get( id_factura,None) != None:
+#             print("Factura Registrada anterioermete")
+#             return
+#     info_Factura ={}
+#     info_Factura["fecha"]=input("Fecha")
+#     info_Factura["Nombre"]=input("Nombre")   
+#     info_Factura["Telefono"]= input("Telefono") 
+#     producto = {}
+#     info_Factura[producto]={}
     
-##################################################################################3
-    id_ropa = input("Id del producto a comprar:  ")
-    Cuentos_tipos_de_propducto = int(input("Cuentos tipos de propducto"))
-    for i in range(Cuentos_tipos_de_propducto):
-        info_Factura[producto][id_ropa] = input("Id Del producto:  ")
-        elementos = cargar_datos_elementos()
-        for elemento in elementos:
-            if elemento.get(id_ropa,None)==None:
-                print('Elemento  No Registrado')
-                return            
-            Numero_de_productos= "Producto N " + input(f"Del siguiente ID {id_ropa} cuantos va a llevar")
-            info_Factura[producto][id_ropa]["Numero de producto"] = Numero_de_productos
-            if elemento.get(id_ropa)==id_ropa:
+# ##################################################################################3
+#     id_ropa = input("Id del producto a comprar:  ")
+#     Cuentos_tipos_de_propducto = int(input("Cuentos tipos de propducto"))
+#     for i in range(Cuentos_tipos_de_propducto):
+#         info_Factura[producto][id_ropa] = input("Id Del producto:  ")
+#         elementos = cargar_datos_elementos()
+#         for elemento in elementos:
+#             if elemento.get(id_ropa,None)==None:
+#                 print('Elemento  No Registrado')
+#                 return            
+#             Numero_de_productos= "Producto N " + input(f"Del siguiente ID {id_ropa} cuantos va a llevar")
+#             info_Factura[producto][id_ropa]["Numero de producto"] = Numero_de_productos
+#             if elemento.get(id_ropa)==id_ropa:
                 
+                
+                
+                
+# Registro_Prenda_Nueva()
