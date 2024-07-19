f37b65dbbf6cd3f94985ff4f935fbd1fbb53803c
AlejandroRinconPerez <alejandro.ingenieria.estudios@gmail.com>
2024-07-19 17:57:00 -0500

    Actulizacion inventario Marca Talla Estadso

--- Registro_Items.py
+++ Registro_Items.py
@@ -1,6 +1,7 @@
 # cargar Datos de los elementos
 from Datos_Elementos import*
 from Datos_Facturas import*
+import datetime
 
 
 
@@ -26,35 +27,40 @@ def Registro_Prenda_Nueva():
     return
         
 
-# def Facturas ():
-#     facturas = cargar_datos_facturas()
-#     id_factura = input("Id De la FACtura:  ")
-#     for factura in facturas:
-#         if factura.get( id_factura,None) != None:
-#             print("Factura Registrada anterioermete")
-#             return
-#     info_Factura ={}
-#     info_Factura["fecha"]=input("Fecha")
-#     info_Factura["Nombre"]=input("Nombre")   
-#     info_Factura["Telefono"]= input("Telefono") 
-#     producto = {}
-#     info_Factura[producto]={}
+def Facturas ():
+    facturas = cargar_datos_facturas()
+    id_factura = input("Id De la FACtura:  ")
+    for factura in facturas:
+        if factura.get( id_factura,None) != None:
+            print("Factura Registrada anterioermete")
+            return
+    info_Factura ={}
+    while True:
+        fecha_str = input("Ingrese una fecha (dd/mm/yyyy): ")
+        try:
+            fecha = datetime.datetime.strptime(fecha_str, "%d/%m/%Y")
+            return fecha
+        except ValueError:
+            print("Formato de fecha incorrecto. Por favor, intente de nuevo.")
+    info_Factura["Nombre"]=input("Nombre")   
+    info_Factura["Telefono"]= input("Telefono") 
+    producto = {}
+    info_Factura[producto]={}
     
-# ##################################################################################3
-#     id_ropa = input("Id del producto a comprar:  ")
-#     Cuentos_tipos_de_propducto = int(input("Cuentos tipos de propducto"))
-#     for i in range(Cuentos_tipos_de_propducto):
-#         info_Factura[producto][id_ropa] = input("Id Del producto:  ")
-#         elementos = cargar_datos_elementos()
-#         for elemento in elementos:
-#             if elemento.get(id_ropa,None)==None:
-#                 print('Elemento  No Registrado')
-#                 return            
-#             Numero_de_productos= "Producto N " + input(f"Del siguiente ID {id_ropa} cuantos va a llevar")
-#             info_Factura[producto][id_ropa]["Numero de producto"] = Numero_de_productos
-#             if elemento.get(id_ropa)==id_ropa:
+##################################################################################3
+    id_ropa = input("Id del producto a comprar:  ")
+    Cuentos_tipos_de_propducto = int(input("Cuentos tipos de propducto"))
+    for i in range(Cuentos_tipos_de_propducto):
+        info_Factura[producto][id_ropa] = input("Id Del producto:  ")
+        elementos = cargar_datos_elementos()
+        for elemento in elementos:
+            if elemento.get(id_ropa,None)==None:
+                print('Elemento  No Registrado')
+                return            
+            Numero_de_productos= "Producto N " + input(f"Del siguiente ID {id_ropa} cuantos va a llevar")
+            info_Factura[producto][id_ropa]["Numero de producto"] = Numero_de_productos
+            if elemento.get(id_ropa)==id_ropa:
                 
                 
                 
                 
-# Registro_Prenda_Nueva()
